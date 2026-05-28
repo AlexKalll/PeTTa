9ca6f60ec41fd2ba721cfc89941e42bd861955e4
AlexKalll <alexkalalw@gmail.com>
2026-05-28 03:25:22 +0000

    feat:  Implement core date & time library

---
+++ lib/lib_datetime.pl
@@ -0,0 +1,29 @@
+% Small Date & Time helpers for PeTTa.
+
+now(TimeStamp) :-
+    get_time(TimeStamp).
+
+to_atom(Value, Atom) :-
+    atom(Value), !,
+    Atom = Value.
+to_atom(Value, Atom) :-
+    string(Value), !,
+    atom_string(Atom, Value).
+to_atom(Value, Atom) :-
+    term_to_atom(Value, Atom).
+
+format_date(TimeStamp, Format, Formatted) :-
+    to_atom(Format, FormatAtom),
+    stamp_date_time(TimeStamp, DateTime, local),
+    format_time(atom(Formatted), FormatAtom, DateTime).
+
+date_diff(From, To, DiffSeconds) :-
+    DiffSeconds is To - From.
+
+date_add(TimeStamp, Seconds, NewTimeStamp) :-
+    NewTimeStamp is TimeStamp + Seconds.
+
+day_of_week(TimeStamp, DayName) :-
+    stamp_date_time(TimeStamp, DateTime, local),
+    format_time(atom(DayAtom), '%A', DateTime),
+    downcase_atom(DayAtom, DayName).
